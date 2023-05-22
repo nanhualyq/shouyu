4aac4c204f0240228abd1d6429deef868d94bc79
lyq <nanhualyq@gmail.com>
2023-05-22 20:50:26 +0800

    feat: nuxt-auth basic setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    modules: ['@sidebase/nuxt-auth'],
+    auth: {
+        isEnabled: true,
+        // NOT FUCKING WORK
+        // globalAppMiddleware: true,
+        // globalMiddlewareOptions: {
+        //     // Whether to allow access to 404 pages without authentication. Set this to `false` to force users to sign-in before seeing `404` pages. Setting this to false may lead to vue-router problems (as the target page does not exist)        
+        //     allow404WithoutAuth: true,
+        //     // Whether to automatically set the callback url to the page the user tried to visit when the middleware stopped them. This is useful to disable this when using the credentials provider, as it does not allow a `callbackUrl`. Setting this to a string-value will result in that being used as the callbackUrl path. Note: You also need to set the global `addDefaultCallbackUrl` setting to `false` if you want to fully disable this for the global middleware.        
+        //     addDefaultCallbackUrl: true
+        // }
+    }
 })
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>
+        status {{ status }}
+    </p>
+    <p>
+        data {{ data }}
+    </p>
+</template>
+
+<script setup>
+// definePageMeta({ middleware: 'auth' })
+// file: e.g ~/pages/login.vue
+const { status, data, signIn, signOut } = useAuth()
+
+// status.value // Session status: `unauthenticated`, `loading`, `authenticated`
+// data.value // Session data, e.g., expiration, user.email, ...
+
+// await signIn() // Sign in the user
+// await signOut() // Sign out the user
+</script>
