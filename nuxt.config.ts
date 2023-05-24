7ef0dfda6801c6c9c7027f401d1d7d41a42f3937
lyq <nanhualyq@gmail.com>
2023-05-24 12:16:42 +0800

    feat: basic auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     runtimeConfig: {
-        sqliteFile: 'db/sqlite/data.db'
+        sqliteFile: 'db/sqlite/data.db',
+        // user1:pass\nuser2:pass
+        basicAuth: '',
     }
 })
