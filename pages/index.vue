f01d1ddc4ab7ed404b136c2181ccd4bd34dc5b5c
lyq <nanhualyq@gmail.com>
2023-05-24 12:10:53 +0800

    feat: delete nuxt-auth

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,7 @@
 <template>
-    <p>
-        status {{ status }}
-    </p>
-    <p>
-        data {{ data }}
-    </p>
     <p>{{ name }}</p>
 </template>
 
 <script setup>
-const { status, data, signIn, signOut } = useAuth()
 const { data: name } = await useLazyFetch('/api/user')
-// status.value // Session status: `unauthenticated`, `loading`, `authenticated`
-// data.value // Session data, e.g., expiration, user.email, ...
-
-// await signIn() // Sign in the user
-// await signOut() // Sign out the user
 </script>
