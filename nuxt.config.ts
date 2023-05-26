653931631c19b998511a5377da0672b3a8b5fa18
lyq <nanhualyq@gmail.com>
2023-05-26 20:22:15 +0800

    feat: twcss & daisyUI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    modules: [
+        '@nuxtjs/tailwindcss'
+    ],
     runtimeConfig: {
         sqliteFile: 'db/data.db',
         // user1:pass\nuser2:pass
