faa39eda360011b8e41f534b200797e1ab88c7df
lyq <nanhualyq@gmail.com>
2023-05-27 19:10:45 +0800

    feat: layout init

---
+++ components/NavbarMenu.vue
@@ -0,0 +1,10 @@
+<template>
+    <li> <NuxtLink to="/">首页</NuxtLink> </li>
+    <li> <NuxtLink to="/book">材料</NuxtLink> </li>
+</template>
+
+<style lang="postcss">
+.router-link-active {
+    @apply active;
+}
+</style>
