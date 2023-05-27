3a0979e6e0e079d438224e2226ddcd8e0b7c2dc6
lyq <nanhualyq@gmail.com>
2023-05-27 17:17:28 +0800

    feat: sql

--- db/sql/3-card.sql
+++ db/sql/3-card.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS card (
     id INTEGER,
+    book_id INTEGER NOT NULL REFERENCES book,
     sentence_id INTEGER NOT NULL REFERENCES sentence,
     skill TEXT NOT NULL CHECK( skill IN('read', 'write', 'listen', 'speak') ),
     create_time TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
