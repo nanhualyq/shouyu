992ed436ebf4b7793c6ffb216a6e9bf88aea1632
lyq <nanhualyq@gmail.com>
2023-05-30 18:18:55 +0800

    feat: use attr class

--- components/NavbarMenu.vue
+++ components/NavbarMenu.vue
@@ -1,10 +1,4 @@
 <template>
-    <li> <NuxtLink to="/">首页</NuxtLink> </li>
-    <li> <NuxtLink to="/book">材料</NuxtLink> </li>
+    <li> <NuxtLink activeClass="active" to="/">首页</NuxtLink> </li>
+    <li> <NuxtLink activeClass="active" to="/book">材料</NuxtLink> </li>
 </template>
-
-<style lang="postcss">
-.router-link-active {
-    @apply active;
-}
-</style>
