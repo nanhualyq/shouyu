f36ef7a5c054ecae53274543c757e8cba0745420
lyq <nanhualyq@gmail.com>
2023-06-21 10:40:02 +0800

    feat: Loading component

---
+++ components/TheLoading.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex justify-center">
+        <span class="loading"></span>
+    </div>
+</template>
+<style scoped lang="postcss">
+.full {
+    @apply absolute top-0 right-0 bottom-0 left-0 bg-black bg-opacity-10;
+}
+</style>
