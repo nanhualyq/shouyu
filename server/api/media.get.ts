51093418f36bf1867120dd2ccb4953b8a9e03298
lyq <nanhualyq@gmail.com>
2023-06-21 21:29:12 +0800

    fix: auto postfix

--- server/api/media.get.ts
+++ server/api/media.get.ts
@@ -6,9 +6,10 @@ ffmpeg.setFfmpegPath(path)
 
 export default defineEventHandler(async event => {
     const { media_url, media_start, media_end } = getQuery(event)
-    const {mediaPath} = useRuntimeConfig()
+    const [, postfix] = String(media_url)?.match(/(\.\w+)$/) || []
+    const { mediaPath } = useRuntimeConfig()
     const url = mediaPath + media_url
-    const temFile = '/tmp/' + Date.now() + '.mp3'
+    const temFile = '/tmp/' + Date.now() + postfix
     await new Promise((resolve, reject) => {
         ffmpeg(url)
             .inputOptions([
