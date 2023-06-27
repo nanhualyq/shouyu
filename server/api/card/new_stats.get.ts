610143c5fe62050d6f57042d25d20dbb51502212
lyq <nanhualyq@gmail.com>
2023-06-27 13:09:28 +0800

    fix: new stats query sql error

--- server/api/card/new_stats.get.ts
+++ server/api/card/new_stats.get.ts
@@ -1,5 +1,10 @@
 import db from "~/db";
 
 export default defineEventHandler(async event => {
-    return db.prepare('select count(card.id) as count,skill from card left join sentence on (card.sentence_id = sentence.id) where sentence.book_id = ? AND skilled IS NULL group by card.skill').all(getQuery(event)?.book_id)
+    return db.prepare(`select 
+    count(card.id) as count,skill 
+    from card 
+    left join sentence on (card.sentence_id = sentence.id) 
+    where sentence.book_id = ? AND due_time IS NULL group by card.skill`)
+        .all(getQuery(event)?.book_id)
 })
