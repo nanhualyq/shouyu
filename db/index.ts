eb3a7cb19ec5f63f7cf3bedb41b12ee325532eb2
lyq <nanhualyq@gmail.com>
2023-07-11 17:20:27 +0800

    feat: migration script

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,4 @@
 import Database from 'better-sqlite3';
-import fs from 'fs'
 
 const config = useRuntimeConfig()
 const db = new Database(config.sqliteFile, { verbose: console.log });
@@ -11,28 +10,3 @@ process.on('SIGINT', () => process.exit(128 + 2));
 process.on('SIGTERM', () => process.exit(128 + 15));
 
 export default db
-
-// migration
-if (process.env.NODE_ENV === 'development') {
-    function getVersionNumber(filename: string) {
-        return +(filename?.match(/^\d+/)?.[0] || '')
-    }
-
-    let version = ''
-    try {
-        version = fs.readFileSync('db/version', 'utf-8')
-    } catch (error) {
-        version = ''
-    }
-    const sqlFiles = fs.readdirSync('db/sql')
-    sqlFiles.sort((a, b) => getVersionNumber(a) - getVersionNumber(b))
-
-    for (const file of sqlFiles) {
-        if (getVersionNumber(file) <= getVersionNumber(version)) {
-            continue
-        }
-        const sql = fs.readFileSync(`db/sql/${file}`, 'utf-8')
-        db.exec(sql)
-        fs.writeFileSync('db/version', file)
-    }
-}
---
+++ db/migration.js
@@ -0,0 +1,26 @@
+const Database = require('better-sqlite3')
+const fs = require('fs')
+
+const db = new Database('db/sqlite/data.db', { verbose: console.log });
+
+function getVersionNumber(filename) {
+    return +(filename?.match(/^\d+/)?.[0] || '')
+}
+
+let version = ''
+try {
+    version = fs.readFileSync('db/version', 'utf-8')
+} catch (error) {
+    version = ''
+}
+const sqlFiles = fs.readdirSync('db/sql')
+sqlFiles.sort((a, b) => getVersionNumber(a) - getVersionNumber(b))
+
+for (const file of sqlFiles) {
+    if (getVersionNumber(file) <= getVersionNumber(version)) {
+        continue
+    }
+    const sql = fs.readFileSync(`db/sql/${file}`, 'utf-8')
+    db.exec(sql)
+    fs.writeFileSync('db/version', file)
+}
