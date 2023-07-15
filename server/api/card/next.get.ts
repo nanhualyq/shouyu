152b4b73d9c6f1ec7be164feea6906e8f64e7d02
lyq <nanhualyq@gmail.com>
2023-07-15 20:23:03 +0800

    feat: next card priority minute due

--- server/api/card/next.get.ts
+++ server/api/card/next.get.ts
@@ -1,5 +1,14 @@
 import db from "~/db";
 
+function queryNextCard(where: string, order: string) {
+    const sql = `SELECT *, count(*) as total FROM (
+        SELECT * FROM card 
+        LEFT JOIN sentence on (sentence.id = card.sentence_id)
+        ${where} ${order} 
+    )`
+    return db.prepare(sql).expand().get()
+}
+
 export default defineEventHandler(async event => {
     const query = await getQuery(event)
     let where = []
@@ -13,28 +22,44 @@ export default defineEventHandler(async event => {
     const { skills, random } = query
     if (skills) {
         const str = (skills as String)
-        .split(',')
-        .map(s => `'${s}'`)
-        .join(',')
+            .split(',')
+            .map(s => `'${s}'`)
+            .join(',')
         where.push(`skill in (${str})`)
     }
+
+    // 分钟级别的到期卡
+    const minuteCardWhere = [
+        ...where,
+        'datetime(due_time) <= datetime()',
+        'skilled = 0'
+    ]
+    whereStr = `where ${minuteCardWhere.join(' AND ')}`
+    order = 'ORDER BY due_time'
+    const minuteCard = queryNextCard(whereStr, order) as any
+
+    // 天数级别的到期卡
     if (query.new) {
-        where.push(`due_time IS NULL`)
+        where.push(`due_time IS NULL OR skilled = 0`)
     }
     if (query.review) {
         where.push(`date(due_time) <= date()`)
     }
-    if (where.length) {
-        whereStr = `where ${where.join(' AND ')}`
-    }
     if (random) {
         order = 'ORDER BY random()'
     }
-    return db.prepare(`SELECT *, count(*) as total FROM (
-        SELECT * FROM card 
-        LEFT JOIN sentence on (sentence.id = card.sentence_id)
-        ${whereStr} ${order} 
-    )`)
-        .expand()
-        .get()
+    if (where.length) {
+        whereStr = `where ${where.join(' AND ')}`
+    }
+    const dayCard = queryNextCard(whereStr, order) as any
+
+    // 优先返回分钟级别的卡片
+    if (minuteCard?.$?.total) {
+        return {
+            ...minuteCard,
+            $: dayCard?.$
+        }
+    } else {
+        return dayCard
+    }
 })
