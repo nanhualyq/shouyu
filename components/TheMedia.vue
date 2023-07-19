5a40d40787f701963f9c1c71e9eb0077005149ce
lyq <nanhualyq@gmail.com>
2023-07-19 19:35:10 +0800

    fix: check media url params

--- components/TheMedia.vue
+++ components/TheMedia.vue
@@ -1,7 +1,15 @@
 <template>
     <!-- may be video -->
-    <audio ref="mediaRef" :class="{loading: pending}" :src="mediaUrl" controls autoplay @loadstart="onStart" @canplaythrough="pending = false" @error="onError"></audio>
-    <p v-if="error" class="text-error">{{ mediaUrl }} 加载失败!</p>
+    <div v-if="paramsError" class="alert alert-error">
+        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+        <span>{{ paramsError }}</span>
+    </div>
+    <audio v-else ref="mediaRef" :class="{ loading: pending }" :src="mediaUrl" controls autoplay @loadstart="onStart"
+        @canplaythrough="pending = false" @error="onError"></audio>
+    <p v-if="loadError" class="text-error">{{ mediaUrl }} 加载失败!</p>
 </template>
 <script setup>
 const { sentence } = defineProps({
@@ -12,14 +20,29 @@ const mediaUrl = computed(() => {
     const search = new URLSearchParams({ media_url, media_start, media_end })
     return '/api/media?' + search.toString()
 })
+const paramsError = computed(() => {
+    for (const key of ['media_url', 'media_start', 'media_end']) {
+        if (!sentence[key]) {
+            return `${key} is empty`
+        }
+    }
+    const { media_start, media_end } = sentence || {}
+    if (!isFinite(media_start) || media_start < 0) {
+        return 'media_start is wrong'
+    }
+    if (!isFinite(media_end) || media_end <= media_start) {
+        return 'media_end is wrong'
+    }
+})
 let pending = ref(false)
-let error = ref(false)
+let loadError = ref(false)
 function onStart() {
-    error.value = false
+    loadError.value = false
     pending.value = true
 }
-function onError() {
-    error.value = true
+function onError(e) {
+    console.log(e);
+    loadError.value = true
     pending.value = false
 }
 const mediaRef = ref(null)
