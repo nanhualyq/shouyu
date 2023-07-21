5a06256657b0e890ee097d35fc96090f518a815a
lyq <nanhualyq@gmail.com>
2023-07-21 12:12:32 +0800

    fix: alert-* class not working

--- components/TheToast.vue
+++ components/TheToast.vue
@@ -3,6 +3,8 @@
         <div class="alert" v-for="row in data" :class="[row.type && `alert-${row.type}`]">
             <span>{{ row.content }}</span>
         </div>
+        <!-- try fix alert-* not working -->
+        <div class="alert hidden alert-info alert-success alert-warning alert-error"></div>
     </div>
 </template>
 
