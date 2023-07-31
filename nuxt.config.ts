272269178434a93453c89d6dc6670b5b4eda3175
lyq <nanhualyq@gmail.com>
2023-07-31 20:45:47 +0800

    feat: nuxt devtool

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,15 +3,21 @@ export default defineNuxtConfig({
     modules: [
         '@nuxtjs/tailwindcss'
     ],
+
     app: {
         head: {
             title: `授渔${process.env.NODE_ENV === 'production' ? '' : ' - ' + process.env.NODE_ENV}`
         }
     },
+
     runtimeConfig: {
         sqliteFile: 'db/sqlite/data.db',
         // user1:pass\nuser2:pass
         basicAuth: '',
         mediaPath: 'media'
+    },
+
+    devtools: {
+        enabled: true
     }
 })
