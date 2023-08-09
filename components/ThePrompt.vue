d9b3e18fe2b71c9fce4531da64c5294bd9edb4c5
lyq <nanhualyq@gmail.com>
2023-08-09 13:14:05 +0800

    fix: myPrompt not working after pressed enter

--- components/ThePrompt.vue
+++ components/ThePrompt.vue
@@ -1,13 +1,13 @@
 <template>
     <dialog class="modal" :open="options.open">
-        <form method="dialog" class="modal-box">
+        <form method="dialog" class="modal-box" @submit.prevent="options.open = false">
             <h3 class="font-bold text-lg">{{ options.tip }}</h3>
             <p class="py-4">
-                <input type="text" class="input input-bordered w-full max-w-xs" v-model="options.value" />
+                <input type="text" class="input input-bordered w-full max-w-xs" v-model="options.value" ref="inputRef" />
             </p>
             <div class="modal-action">
                 <button type="button" class="btn" @click="options = {}">取消</button>
-                <button type="button" class="btn btn-primary" @click="options.open = false">确认</button>
+                <button type="submit" class="btn btn-primary">确认</button>
             </div>
         </form>
     </dialog>
@@ -19,4 +19,10 @@ const options = useState('the-prompt-options', () => ref({
     value: '',
     tip: ''
 }))
+const inputRef = ref(null)
+watch(options, (val, oldVal) => {
+    if (val.open) {
+        inputRef.value?.focus()
+    }
+}, { deep: true })
 </script>
